96a05faee69ba6e735fca72f50e3aaf1dd66d74c
zdroid9770 <zdroid9770@yahoo.com>
2013-12-03 21:41:20 -0600

    Updates
    	- Removed 2013-06-10 update
    	- Added MySQL.exe

--- updates/2013_06_10_01_creatures.sql
+++
@@ -1,3 +0,0 @@
-/* Waypoint (Only GM can see it) */
-INSERT INTO `arc_world`.`creature_names` (`entry`, `name`, `subname`, `male_displayid`) VALUES (1, 'Waypoint (Only GM can see it)', 'Visual', 10045);
-INSERT INTO `arc_world`.`creature_proto` (`entry`, `minlevel`, `maxlevel`, `minhealth`, `maxhealth`, `scale`) VALUES (1, 1, 80, 1, 1, 1);
