4bd7cb5f7536a32208579ff966c41c78280addb9
zdroid9770 <zdroid9770@yahoo.com>
2013-12-09 16:30:30 -0600

    Additional files
    	- Terms of Use Agreement
    	- misc. Tools

---
+++ tools/ArcEmu/import_arcemu.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# LoE DB Linux Tool
+#
+# Linux Tool v2.0 by Latheesan
+# Modified by LoE 2012-2013 
+############################################################################
+#
+#  Tool Configuration
+#
+#  user - MYSQL username
+#  pass - MYSQL password
+#  wdb  - Your world database
+#
+############################################################################
+server="localhost"
+port="3306"
+user="root"
+pass="password"
+world_db="arc_world"
+
+
+############################################################################
+#
+#  Server configuration, do not edit 
+#
+############################################################################
+
+sqlpath="arcemu_world"
+loc_deDE="localizations/deDE"
+
+
+############################################################################
+#
+#  Method to print tool's logo
+#
+############################################################################
+logo() {
+	clear
+	echo
+	echo " dP                  88888888b    dP   dP   dP                   dP       dP "
+	echo " 88                  88           88   88   88                   88       88 "
+	echo " 88        .d8888b. a88aaaa       88  .8P  .8P .d8888b. 88d888b. 88 .d888b88 "
+	echo " 88        88'  '88  88           88  d8'  d8' 88'  '88 88'  '88 88 88'  '88 "
+	echo " 88        88.  .88  88           88.d8P8.d8P  88.  .88 88       88 88.  .88 "
+	echo " 88888888P '88888P'  88888888P    8888' Y88'   '88888P' dP       dP '88888P8 "
+	echo
+	echo " Developer website: dev.LandOfElves.net"
+	echo " Developer forum:   forum.LandOfElves.net"
+	echo 
+}
+
+
+############################################################################
+#
+#  Main program
+#
+############################################################################
+until [ "${option}" = "x" ]; do
+	logo
+	echo " i  - Install a clean LoE DB world database"
+        echo "      (Warning! The whole world db will be overwritten)"
+        echo " de - Import German Localization"
+        echo "      (Warning! All German localization will be overwritten!)"
+	echo " x  - Exit this tool"
+	echo
+	read -p " Enter option: " option
+	if [ "${option}" = "i" ]; then
+
+		# Clean database
+		echo "DROP DATABASE IF EXISTS ${world_db};" > ${sqlpath}/databaseclean.sql
+		echo "CREATE DATABASE IF NOT EXISTS ${world_db};" >> ${sqlpath}/databaseclean.sql
+		mysql --host ${server} --user=${user} --port=${port} --password=${pass} < ${sqlpath}/databaseclean.sql
+		rm ${sqlpath}/databaseclean.sql
+		echo
+		echo
+		echo " Database ${world_db} successfully created !"
+		
+		max=`ls -1 "${sqlpath}"/*.sql | wc -l`
+		i=0
+		for table in "${sqlpath}"/*.sql; do
+			i=$((${i}+1))
+			echo " [${i}/${max}] import: ${table##*/}"
+			mysql -h ${server} --user=${user} --port=${port} --password=${pass} ${world_db} < "${table}"
+		done
+
+		echo
+		echo " [Importing] Finished..."
+
+	elif [ "${option}" = "de" ]; then
+		echo
+
+		max=`ls -1 "${loc_deDE}"/*.sql | wc -l`
+		i=0
+		for table in "${loc_deDE}"/*.sql; do
+			i=$((${i}+1))
+			echo " [${i}/${max}] import: ${table##*/}"
+			mysql -h ${server} --user=${user} --port=${port} --password=${pass} ${world_db} < "${table}"
+		done
+
+		echo
+		echo " [Importing] Finished..."
+
+
+
+	elif [ "${option}" != "x" ]; then
+		echo
+		read -p " Incorrect option '${option}'."
+		echo
+	fi
+	if [ "${option}" != "x" ]; then
+		echo
+		read -p " Press any key to continue..."
+		echo
+	fi
+	echo " Thanks for using the LoE DB import tool!"
+done
---
+++ tools/ArcPro/import_arcpro.bat
@@ -0,0 +1,194 @@
+@ECHO OFF
+TITLE LoE's Installation Tool
+COLOR 0E
+
+
+SET dumppath=.\dump\
+SET bin=.\bin\
+SET sql=.\arcpro_world
+SET changsql=.\changesets
+SET loc_deDE=.\localizations\deDE
+
+:TOP
+CLS
+ECHO.
+ECHO          ษออออออออออออออออออออออออออออออออออออออป
+ECHO          บ                                      บ
+ECHO          บ         LoE Installation Tool        บ
+ECHO          บ                                      บ
+ECHO          บ  More information:                   บ
+ECHO          บ  Dev-Website: dev.landofelves.net    บ
+ECHO          บ  Dev-Forum:   forum.LandOfElves.net  บ
+ECHO          บ                                      บ
+ECHO          ศออออออออออออออออออออออออออออออออออออออผ
+ECHO.
+ECHO.
+ECHO    Please enter your MySQL Info...
+ECHO.
+SET /p host= MySQL Server Address (e.g. 127.0.0.1): 
+SET /p port= MySQL Server Port (e.g. 3306): 
+ECHO.
+SET /p user= MySQL Username: 
+SET /p pass= MySQL Password: 
+ECHO.
+SET /p world_db= Name of World Database: 
+
+:Begin
+CLS
+SET v=""
+ECHO.
+ECHO  dP                  88888888b    dP   dP   dP                   dP       dP 
+ECHO  88                  88           88   88   88                   88       88 
+ECHO  88        .d8888b. a88aaaa       88  .8P  .8P .d8888b. 88d888b. 88 .d888b88 
+ECHO  88        88'  `88  88           88  d8'  d8' 88'  `88 88'  `88 88 88'  `88 
+ECHO  88        88.  .88  88           88.d8P8.d8P  88.  .88 88       88 88.  .88 
+ECHO  88888888P `88888P'  88888888P    8888' Y88'   `88888P' dP       dP `88888P8 
+ECHO.
+ECHO    Developer Website http://dev.LandOfElves.net  
+ECHO    Developer Forum   http://forum.LandOfElves.net  
+ECHO.
+ECHO.
+ECHO    i  - Import LoE World Database
+ECHO         (Warning! The whole world db will be overwritten!)
+ECHO    de - Import German Localization
+ECHO         (Warning! All German localization will be overwritten!)
+REM ECHO    U - Import Changeset.
+ECHO.
+ECHO    s - Change your settings
+ECHO.
+ECHO    x - Exit this tool
+ECHO.
+SET /p v= 		Enter your choice: 
+IF %v%==* GOTO error
+IF %v%==i GOTO import
+IF %v%==de GOTO import_de
+REM IF %v%==u GOTO changeset
+REM IF %v%==d GOTO dumpever
+IF %v%==s GOTO top
+IF %v%==x GOTO exit
+IF %v%=="" GOTO exit
+GOTO error
+
+
+:import
+CLS
+ECHO First Lets Create database (or overwrite old) !!
+ECHO.
+
+REM Create databaseclean.sql with SQL queries
+ECHO DROP DATABASE IF EXISTS `%world_db%`; > %sql%\databaseclean.sql
+ECHO CREATE DATABASE IF NOT EXISTS `%world_db%`; >> %sql%\databaseclean.sql
+
+REM Clean database
+%bin%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% < %sql%\databaseclean.sql
+@DEL %sql%\databaseclean.sql
+ECHO Database %world_db% successfully created !
+ECHO.
+
+ECHO Importing files now...
+ECHO.
+FOR %%C IN (%sql%\*.sql) DO (
+	ECHO Importing: %%~nxC
+	%bin%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < "%%~fC"
+	ECHO Successfully imported %%~nxC
+	ECHO.
+)
+ECHO Done !
+ECHO.
+ECHO.
+ECHO You don't have to import any changesets or updates now!
+ECHO.
+ECHO.
+PAUSE
+GOTO exit
+
+
+:import_de
+ECHO.
+ECHO Importing German localization files now...
+ECHO.
+FOR %%C IN (%loc_deDE%\*.sql) DO (
+	ECHO Importing: %%~nxC
+	%bin%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < "%%~fC"
+	ECHO Successfully imported %%~nxC
+	ECHO.
+)
+ECHO Done.
+ECHO.
+ECHO.
+ECHO You don't have to import any changesets or updates now!
+ECHO.
+ECHO.
+PAUSE
+GOTO exit
+
+
+:changeset
+CLS
+ECHO   Please Write down number of changeset (not the number of rev!!!)
+ECHO   Or type in "a" to import all changesets
+ECHO.
+ECHO.
+set /p ch=      Number: 
+ECHO.
+IF %ch%==a GOTO changesetall
+ECHO      Importing...
+IF NOT EXIST "%changsql%\changeset_%ch%.sql" GOTO error2
+ECHO.
+%bin%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < %changsql%\changeset_%ch%.sql
+ECHO.
+ECHO      File changeset_%ch%.sql imported sucesfully!
+ECHO.
+PAUSE
+GOTO begin
+
+:changesetall
+CLS
+ECHO.
+ECHO import: Changesets
+for %%C in (development\changesets\*.sql) do (
+	ECHO import: %%~nxC
+	%bin%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < "%%~fC"
+)
+ECHO Changesets imported sucesfully!
+ECHO.
+PAUSE   
+GOTO begin
+
+:dumpever
+CLS
+ECHO.
+ECHO          Type there name of table, which you want to dump
+ECHO.
+SET z=""
+SET /p z=           Name: 
+IF %z%=="" GOTO error
+ECHO              Processing....
+ECHO.
+IF NOT EXIST "%dumppath%" MKDIR %dumppath%
+
+%bin%\mysqldump --host=%host% --user=%user% --password=%pass% --add-drop-table %world_db% "%z%" > "%dumppath%\%z%.sql"
+
+ECHO              Dump table %z% from database %world_db% is successful !
+ECHO.
+SET y=""
+SET /p y=          Dump another table (Y/N)?: 
+IF %y%==y GOTO dumpever
+IF %y%==Y GOTO dumpever
+IF %y%==n GOTO begin
+IF %y%==N GOTO begin
+GOTO dumpever
+
+:error
+ECHO	Please enter a correct character.
+ECHO.
+PAUSE
+GOTO begin
+
+:error2
+ECHO	Changeset with this number not found.
+ECHO.
+PAUSE
+GOTO begin
+
+:exit
