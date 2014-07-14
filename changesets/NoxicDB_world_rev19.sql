ab69eb42086b6ec60395dd339915b0308ceec9e2
zdroid9770 <zdroid9770@yahoo.com>
2014-07-14 13:30:46 -0500

    NoxicDB - Revision 19

---
+++ changesets/NoxicDB_world_rev19.sql
@@ -0,0 +1,72 @@
+/**********************************************
+ NoxicDB change set Rev. 19
+ 14. July, 2014
+**********************************************/
+
+ALTER TABLE `playercreateinfo`
+	ADD COLUMN `orientation` FLOAT NOT NULL DEFAULT '0' AFTER `positionZ`;
+
+UPDATE `playercreateinfo` SET `positionX`=-8949.95, `positionY`=-132.493, `positionZ`=83.5312 WHERE  `Index`=1;
+UPDATE `playercreateinfo` SET `positionX`=-8949.95, `positionY`=-132.493, `positionZ`=83.5312 WHERE  `Index`=9;
+UPDATE `playercreateinfo` SET `positionX`=-8949.95, `positionY`=-132.493, `positionZ`=83.5312 WHERE  `Index`=18;
+UPDATE `playercreateinfo` SET `positionX`=-8949.95, `positionY`=-132.493, `positionZ`=83.5312 WHERE  `Index`=25;
+UPDATE `playercreateinfo` SET `positionX`=-8949.95, `positionY`=-132.493, `positionZ`=83.5312 WHERE  `Index`=34;
+UPDATE `playercreateinfo` SET `positionX`=-8949.95, `positionY`=-132.493, `positionZ`=83.5312 WHERE  `Index`=38;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=2;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=11;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=19;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=30;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=39;
+UPDATE `playercreateinfo` SET `positionX`=-6240.32, `positionY`=331.033, `positionZ`=382.758, `orientation`=6.17716 WHERE  `Index`=3;
+UPDATE `playercreateinfo` SET `positionX`=-6240.32, `positionY`=331.033, `positionZ`=382.758, `orientation`=6.17716 WHERE  `Index`=10;
+UPDATE `playercreateinfo` SET `positionX`=-6240.32, `positionY`=331.033, `positionZ`=382.758, `orientation`=6.17716 WHERE  `Index`=12;
+UPDATE `playercreateinfo` SET `positionX`=-6240.32, `positionY`=331.033, `positionZ`=382.758, `orientation`=6.17716 WHERE  `Index`=20;
+UPDATE `playercreateinfo` SET `positionX`=-6240.32, `positionY`=331.033, `positionZ`=382.758, `orientation`=6.17716 WHERE  `Index`=26;
+UPDATE `playercreateinfo` SET `positionX`=10311.3, `positionY`=832.463, `positionZ`=1326.41, `orientation`=5.69632 WHERE  `Index`=4;
+UPDATE `playercreateinfo` SET `positionX`=10311.3, `positionY`=832.463, `positionZ`=1326.41, `orientation`=5.69632 WHERE  `Index`=15;
+UPDATE `playercreateinfo` SET `positionX`=10311.3, `positionY`=832.463, `positionZ`=1326.41, `orientation`=5.69632 WHERE  `Index`=21;
+UPDATE `playercreateinfo` SET `positionX`=10311.3, `positionY`=832.463, `positionZ`=1326.41, `orientation`=5.69632 WHERE  `Index`=27;
+UPDATE `playercreateinfo` SET `positionX`=10311.3, `positionY`=832.463, `positionZ`=1326.41, `orientation`=5.69632 WHERE  `Index`=42;
+UPDATE `playercreateinfo` SET `positionX`=1676.71, `positionY`=1678.31, `orientation`=2.70526 WHERE  `Index`=5;
+UPDATE `playercreateinfo` SET `positionX`=1676.71, `positionY`=1678.31, `orientation`=2.70526 WHERE  `Index`=22;
+UPDATE `playercreateinfo` SET `positionX`=1676.71, `positionY`=1678.31, `orientation`=2.70526 WHERE  `Index`=28;
+UPDATE `playercreateinfo` SET `positionX`=1676.71, `positionY`=1678.31, `orientation`=2.70526 WHERE  `Index`=35;
+UPDATE `playercreateinfo` SET `positionX`=1676.71, `positionY`=1678.31, `orientation`=2.70526 WHERE  `Index`=40;
+UPDATE `playercreateinfo` SET `positionZ`=52.9968 WHERE  `Index`=6;
+UPDATE `playercreateinfo` SET `positionZ`=52.9968 WHERE  `Index`=16;
+UPDATE `playercreateinfo` SET `positionZ`=52.9968 WHERE  `Index`=31;
+UPDATE `playercreateinfo` SET `positionZ`=52.9968 WHERE  `Index`=43;
+UPDATE `playercreateinfo` SET `positionX`=-6240, `positionY`=331, `positionZ`=383 WHERE  `Index`=7;
+UPDATE `playercreateinfo` SET `positionX`=-6240, `positionY`=331, `positionZ`=383 WHERE  `Index`=23;
+UPDATE `playercreateinfo` SET `positionX`=-6240, `positionY`=331, `positionZ`=383 WHERE  `Index`=62;
+UPDATE `playercreateinfo` SET `positionX`=-6240, `positionY`=331, `positionZ`=383 WHERE  `Index`=41;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=8;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=17;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=24;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=29;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=32;
+UPDATE `playercreateinfo` SET `positionX`=-618.518, `positionY`=-4251.67, `positionZ`=38.718 WHERE  `Index`=37;
+UPDATE `playercreateinfo` SET `positionX`=10349.6, `positionY`=-6357.29, `positionZ`=33.4026, `orientation`=5.31605 WHERE  `Index`=50;
+UPDATE `playercreateinfo` SET `positionX`=10349.6, `positionY`=-6357.29, `positionZ`=33.4026, `orientation`=5.31605 WHERE  `Index`=51;
+UPDATE `playercreateinfo` SET `positionX`=10349.6, `positionY`=-6357.29, `positionZ`=33.4026, `orientation`=5.31605 WHERE  `Index`=52;
+UPDATE `playercreateinfo` SET `positionX`=10349.6, `positionY`=-6357.29, `positionZ`=33.4026, `orientation`=5.31605 WHERE  `Index`=53;
+UPDATE `playercreateinfo` SET `positionX`=10349.6, `positionY`=-6357.29, `positionZ`=33.4026, `orientation`=5.31605 WHERE  `Index`=54;
+UPDATE `playercreateinfo` SET `positionX`=10349.6, `positionY`=-6357.29, `positionZ`=33.4026, `orientation`=5.31605 WHERE  `Index`=55;
+UPDATE `playercreateinfo` SET `positionX`=-3961.64, `positionZ`=100.615, `orientation`=2.08364 WHERE  `Index`=44;
+UPDATE `playercreateinfo` SET `positionX`=-3961.64, `positionZ`=100.615, `orientation`=2.08364 WHERE  `Index`=45;
+UPDATE `playercreateinfo` SET `positionX`=-3961.64, `positionZ`=100.615, `orientation`=2.08364 WHERE  `Index`=46;
+UPDATE `playercreateinfo` SET `positionX`=-3961.64, `positionZ`=100.615, `orientation`=2.08364 WHERE  `Index`=47;
+UPDATE `playercreateinfo` SET `positionX`=-3961.64, `positionZ`=100.615, `orientation`=2.08364 WHERE  `Index`=48;
+UPDATE `playercreateinfo` SET `positionX`=-3961.64, `positionZ`=100.615, `orientation`=2.08364 WHERE  `Index`=49;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=56;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=57;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=58;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=59;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=60;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=61;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=62;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=63;
+UPDATE `playercreateinfo` SET `positionX`=2355.84, `positionY`=-5664.77, `orientation`=3.65997 WHERE  `Index`=64;
+
+/* Update the database version to latest version */
+UPDATE db_version SET revision=19 WHERE db_name='NoxicDB';
