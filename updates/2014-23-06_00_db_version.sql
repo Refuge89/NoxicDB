fe10e0c21f16dd1af0b98147d050e9324439c476
zdroid9770 <zdroid9770@yahoo.com>
2014-07-14 13:35:28 -0500

    Changes
    	Move old change set.
    	Fixed a SQL that was not suppose have an IF EXIST statement

--- updates/2014-23-06_00_db_version.sql
+++ updates/2014-23-06_00_db_version.sql
@@ -1,3 +1,3 @@
-DELETE IF EXISTS FROM `db_version` WHERE  `db_name`='ArcPro 3.3.5a';
+DELETE FROM `db_version` WHERE  `db_name`='ArcPro 3.3.5a';
 
 UPDATE `db_version` SET `revision`=16, `changeset`=15, `last_included_arcemu_update`='2014-23-06_00_db_version', `last_db_update_by`='zdroid9770' WHERE  `db_name`='NoxicDB';
